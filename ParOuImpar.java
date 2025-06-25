dc4ce097d374c1aab8ea332886c0a8afeaa46754
Otávio Moreira <otaviootavio2007mg@gmail.com>
2025-06-25 15:36:14 -0300

    Par ou ímpar

---
+++ ParOuImpar.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ParOuImpar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int repetidor = 1;
+        while (repetidor == 1) {
+            System.out.print("Digite o numero: ");
+            int numero = scanner.nextInt();
+
+            if (numero % 2 == 0) {
+                System.out.println("Par");
+            }else if (numero % 2 == 1) {
+                System.out.println("Impar");
+            }else{
+                System.out.println("Erro dos sonhos");
+            }
+        }
+    }
+}
